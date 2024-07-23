4e465df34fd20230a6fc53f284b1984eb40b42d6
Alessandro Andrade <andrade.sp27@gmail.com>
2024-07-22 22:04:11 -0400

    Create cypress project

--- README.md
+++ README.md
@@ -1,28 +1,24 @@
-# 🌲 Cypress, do Zero à Nuvem ☁️
+teste-de-dados-cy
+principal
 
-👋 Seja bem-vindo(a)!
+Projeto de exemplo para demonstrar um cy.dataTestcomando personalizado do Cypress.
 
-É muito bom tê-lo(a) aqui. Tenho certeza que você vai amar esse curso. ❤️
+Pré requisitos
+É necessário ter o Node.js e o npm instalados para executar este projeto.
 
-## O que você vai aprender?
+Eu usei versões v18.15.0e 9.5.0do Node.js e npm, respectivamente. Eu sugiro que você use as mesmas versões ou versões posteriores.
 
-- Como configurar um projeto Cypress do zero
-- Como visitar páginas locais e remotas
-- Como lidar com os elementos mais comuns encontrados em aplicações web
-- Como testar upload de arquivos
-- Como realizar as mais diversas verificações de resultados esperados
-- Como criar comandos customizados
-- Como lidar com links que abrem em outra aba do navegador
-- Como rodar testes simulando as dimensões de um dispositivo móvel
-- Como resolver os mesmos problemas de diferentes formas, conhecendo a [API do Cypress](https://docs.cypress.io/api/table-of-contents)
-- Como criar uma documentação mínima para seu projeto de testes automatizados
-- Como executar os testes em um _workflow_ de integração contínua sempre que mudanças ocorrerem no código da aplicação (ou dos testes)
-- Como integrar seu _workflow_ de integração contínua com o Cypress Cloud (o serviço de gestão de testes do Cypress na nuvem)
+Instalação
+Execute npm install(ou npm ipara a versão curta) para instalar as dependências de desenvolvimento.
 
-## Vamos começar?
+Testes
+Observação: antes de executar os testes, faça uma cópia do cypress.env.example.jsonarquivo como cypress.env.json, que no mundo real você atualizaria com credenciais válidas.
 
-Vá para a seção [estrutura do curso](./lessons/_course-structure_.md).
+O cypress.env.jsonarquivo está incluído .gitignoree você tem certeza de que informações confidenciais não serão versionadas.
 
-___
+Execute npm test(ou npm tpara a versão curta) para executar o teste no modo headless.
 
-Este é um curso da **Escola Talking About Testing**.
+Ou execute npm run cy:openpara abrir o Cypress no modo interativo.
+
+Apoie este projeto
+Se você quiser apoiar este projeto, deixe um ⭐.
---
+++ cypress/e2e/CAC-TAT.cy.js
@@ -0,0 +1,171 @@
+describe('Central de Atendimento ao Cliente TAT', () => {
+  beforeEach(() => {
+    cy.visit('./src/index.html')
+  });
+  it('Verifica o título da aplicação', () => {
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+
+  it('Preenche os campos obrigatórios e envia o formulário', () => {
+    const longText = Cypress._.repeat('abcdefghijklmnopqrstuvwxyz', 10)
+
+    cy.get('#firstName').type('Alessandro')
+    cy.get('#lastName').type('Andrade')
+    cy.get('#email').type('andrade@email.com')
+    cy.get('#open-text-area').type(longText, { delay: 0})
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.success').should('be.visible')
+  });
+
+  it('Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    cy.get('#firstName').type('Alessandro')
+    cy.get('#lastName').type('Andrade')
+    cy.get('#email').type('andrade@email,com')
+    cy.get('#open-text-area').type('Teste')
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.error').should('be.visible')
+  });
+
+  it('Campo de telefone continua vazio quando preenchido com um valor não-numérico', () => {
+    cy.get('#phone')
+      .type('abcde')
+      .should('have.value', '')
+  })
+
+  it('Exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#firstName').type('Alessandro')
+    cy.get('#lastName').type('Andrade')
+    cy.get('#email').type('andrade@email.com')
+    cy.get('#open-text-area').type('Teste')
+    cy.get('#phone-checkbox').check()
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.error').should('be.visible')
+  });
+
+  it('Preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+    cy.get('#firstName')
+      .type('Alessandro')
+      .should('have.value', 'Alessandro')
+      .clear()
+      .should('have.value', '')
+    cy.get('#lastName')
+      .type('Andrade')
+      .should('have.value', 'Andrade')
+      .clear()
+      .should('have.value', '')
+    cy.get('#email')
+      .type('andrade@email.com')
+      .should('have.value', 'andrade@email.com')
+      .clear()
+      .should('have.value', '')
+    cy.get('#phone')
+      .type('123456789')
+      .should('have.value', '123456789')
+      .clear()
+      .should('have.value', '')
+  });
+
+  it('Exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.error').should('be.visible')
+  });
+
+  it('Envia o formuário com sucesso usando um comando customizado', () => {
+    // const data = {
+    //   firstName: 'Alessandro',
+    //   lastName: 'Santos Andrade',
+    //   email: 'andrade@email.com',
+    //   text: 'Teste.'
+    // }
+    cy.fillMandatoryFieldsAndSubmit()
+
+    cy.get('.success').should('be.visible')
+  });
+
+  it('Seleciona um produto (YouTube) por seu texto', () => {
+    cy.get('#product')
+      .select('YouTube')
+      .should('have.value', 'youtube')
+  });
+
+  it('Seleciona um produto (Mentoria) por seu valor (value)', () => {
+    cy.get('#product')
+      .select('mentoria')
+      .should('have.value', 'mentoria')
+  });
+
+  it('Seleciona um produto (Blog) por seu índice', () => {
+    cy.get('#product')
+      .select(1)
+      .should('have.value', 'blog')
+  });
+
+  it('Marca o tipo de atendimento "Feedback"', () => {
+    cy.get('input[type="radio"][value="feedback"]')
+      .check()
+      .should('be.checked')
+  });
+
+  it('Marca cada tipo de atendimento', () => {
+    cy.get('input[type="radio"]')
+      .each(typeOfService => {
+        cy.wrap(typeOfService)
+          .check()
+          .should('be.checked')
+      })
+  });
+
+  it('Marca ambos checkboxes, depois desmarca o último', () => {
+    cy.get('input[type="checkbox"]')
+      .check()
+      .should('be.checked')
+      .last()
+      .uncheck()
+      .should('not.be.checked')
+  });
+
+  it('Seleciona um arquivo da pasta fixtures', () => {
+    cy.get('#file-upload')
+      .selectFile('cypress/fixtures/example.json')
+      .should(input => {
+        expect(input[0].files[0].name).to.equal('example.json')
+      })
+  })
+
+  it('Seleciona um arquivo simulando um drag-and-drop', () => {
+    cy.get('#file-upload')
+      .selectFile('cypress/fixtures/example.json', { action: 'drag-drop' })
+      .should(input => {
+        expect(input[0].files[0].name).to.equal('example.json')
+      })
+  });
+
+  it('Seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+    cy.fixture('example.json').as('sampleFile')
+    cy.get('#file-upload')
+      .selectFile('@sampleFile')
+      .should(input => {
+        expect(input[0].files[0].name).to.equal('example.json')
+      })
+  });
+
+  it('Verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+    cy.contains('a', 'Política de Privacidade')
+      .should('have.attr', 'href', 'privacy.html')
+      .and('have.attr', 'target', '_blank')
+  });
+
+  it('Acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+    cy.contains('a', 'Política de Privacidade')
+      .invoke('removeAttr', 'target')
+      .click()
+
+      cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
+  });
+
+  
+})
